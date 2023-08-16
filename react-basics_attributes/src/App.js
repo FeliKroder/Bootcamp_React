ccb7619ac0a97b73079b8db98c87f127f35c21a7
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-08-16 12:22:15 +0200

    complete challenge

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article className="article">
+      <h2 className="article_title">Hello World</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam
+      </p>
+      <a
+        className="article__link"
+        href="https://www.youtube.com/watch?v=a25_gGnmJAw"
+      >
+        ♪ Hello World | Alan Walker ♪
+      </a>
+      <label id="input"></label>
+      <input htmlFor="input"></input>
+    </article>
+  );
 }
