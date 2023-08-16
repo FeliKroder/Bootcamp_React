a89df7880055c90eeab001032a8afb9cf731f472
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-08-16 11:59:24 +0200

    add one more paragraph and h3

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -24,6 +24,14 @@ function HelloWorldArticle() {
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam
       </p>
+      <h3>Hello Stranger</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet.
+      </p>
     </a>
   );
 }
