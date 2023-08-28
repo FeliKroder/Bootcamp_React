49e51f0b8a05008951b709e8b8ae7236dc7932e8
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-08-28 11:23:41 +0200

    complete challenge

--- react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?offset=0");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(url);
         const data = await response.json();
+        setData(data);
         setPokemon(data.results);
       } catch (error) {
         console.log(error);
@@ -17,12 +18,16 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [url]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => setUrl(data.previous)}>
+        Previous Page
+      </button>
+      <button type="button" onClick={() => setUrl(data.next)}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
